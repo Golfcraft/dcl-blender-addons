172e0074267bb0d85e3795277c6ba4b1ae0ef119
Carlos Muñoz <carlos.damian.munoz@gmail.com>
2022-08-10 02:20:29 -0300

    Add armature picker. Fix classes. Fix context.

--- ohmyverse-dcl-tools/ot_armature_symmetrizer.py
+++ ohmyverse-dcl-tools/ot_armature_symmetrizer.py
@@ -1,6 +1,6 @@
 import bpy
 
-class OT_ArmatureSymmetrizer(bpy.types.Operator):
+class OMV_OT_ArmatureSymmetrizer(bpy.types.Operator):
     """Symmetrize and restore DCL armatures"""
     bl_idname = "dcl.armature_symmetrizer"
     bl_label = "Symmetrize and restore dcl armatures"
@@ -13,32 +13,43 @@ class OT_ArmatureSymmetrizer(bpy.types.Operator):
     action : bpy.props.EnumProperty(items=enum_items)
     @classmethod
     def poll(cls, context):
-        active = bpy.context.active_object
-        selected = bpy.context.selected_objects
-        if (context.area.ui_type == 'VIEW_3D') and (len(selected) == 1) and (active.type == 'ARMATURE'):
+        if (context.area.ui_type == 'VIEW_3D') and context.scene.Armature:
             return True
 
     def execute(self, context):
-        C = bpy.context
-        active = bpy.context.active_object
-        if self.action == 'SYMMETRIZE' and not "SYMMETRIZED_" in active.name:
-            # Fix naming and bone rolling
-            bpy.ops.object.mode_set(mode = 'EDIT')
-            for bone in C.active_object.data.edit_bones:
-                if "_Right" in bone.name:
-                    bone.name = bone.name.replace("_Right", "_") + ".R"
-                    if "Sho" in bone.name \
-                    or "Han" in bone.name \
-                    or "Arm" in bone.name:
-                        bone.roll += 3.14159
-                if "_Left" in bone.name:
-                    bone.name = bone.name.replace("_Left", "_") + ".L"
-            bpy.ops.object.mode_set(mode = 'OBJECT')
-            C.active_object.name = "SYMMETRIZED_" + C.active_object.name
+        # current_active = bpy.context.active_object
+
+        if bpy.context.scene.Armature is not None:
+            active = bpy.context.scene.Armature
+        
+        # bpy.context.active_object = active
+        context.view_layer.objects.active = active
+        
+        bpy.ops.object.mode_set(mode = 'OBJECT')
+        
+        if self.action == 'SYMMETRIZE':
+            if not "SYMMETRIZED_" in active.name:
+                # Fix naming and bone rolling
+                bpy.ops.object.mode_set(mode = 'EDIT')
+                for bone in active.data.edit_bones:
+                    if "_Right" in bone.name:
+                        bone.name = bone.name.replace("_Right", "_") + ".R"
+                        if "Sho" in bone.name \
+                        or "Han" in bone.name \
+                        or "Arm" in bone.name:
+                            bone.roll += 3.14159
+                    if "_Left" in bone.name:
+                        bone.name = bone.name.replace("_Left", "_") + ".L"
+                bpy.ops.object.mode_set(mode = 'OBJECT')
+                active.name = "SYMMETRIZED_" + active.name
+                self.report({'INFO'}, "Successful armature symmetrization")
+            else:    
+                self.report({'WARNING'}, "The armature is already symmetrical")
+
         elif self.action == 'RESTORE_DCL' and "SYMMETRIZED_" in active.name:
             # Fix naming and bone rolling
             bpy.ops.object.mode_set(mode = 'EDIT')
-            for bone in C.active_object.data.edit_bones:
+            for bone in active.data.edit_bones:
                 if ".R" in bone.name:
                     bone.name = bone.name.replace("_", "_Right").replace(".R", "")
                     if "Sho" in bone.name \
@@ -48,7 +59,14 @@ class OT_ArmatureSymmetrizer(bpy.types.Operator):
                 if ".L" in bone.name:
                     bone.name = bone.name.replace("_", "_Left").replace(".L", "")
             bpy.ops.object.mode_set(mode = 'OBJECT')
-            C.active_object.name = C.active_object.name.replace("SYMMETRIZED_", "")
+            active.name = active.name.replace("SYMMETRIZED_", "")
+            self.report({'INFO'}, "DCL armature was restored")
+
+        # Deselect all objects
+        bpy.ops.object.select_all(action='DESELECT')
+        # Set active object to None
+        context.view_layer.objects.active = None
+
         return{'FINISHED'}
 
 
@@ -56,7 +74,7 @@ class OT_ArmatureSymmetrizer(bpy.types.Operator):
 # REGISTER/UNREGISTER
 ####################################
 def register():
-    bpy.utils.register_class(OT_ArmatureSymmetrizer) 
+    bpy.utils.register_class(OMV_OT_ArmatureSymmetrizer) 
         
 def unregister():
-    bpy.utils.unregister_class(OT_ArmatureSymmetrizer)
+    bpy.utils.unregister_class(OMV_OT_ArmatureSymmetrizer)
