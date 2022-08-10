6c6af646ec6aa9e64e513312f44d95eb1aea4954
Carlos Muñoz <carlos.damian.munoz@gmail.com>
2022-08-10 02:22:45 -0300

    Add Armature picker.

--- ohmyverse-dcl-tools/pt_ohmyverse_dcl_tools.py
+++ ohmyverse-dcl-tools/pt_ohmyverse_dcl_tools.py
@@ -4,9 +4,19 @@
 
 import bpy
 
-class PT_OhmyverseDclTools(bpy.types.Panel):
+from bpy.props import (
+    BoolProperty,
+    FloatProperty,
+    StringProperty,
+    EnumProperty,
+    PointerProperty,
+    CollectionProperty,
+    IntProperty
+)
+
+class OMV_PT_OhmyverseDclTools(bpy.types.Panel):
     bl_label = "Ohmyverse DCL Tools"
-    bl_idname = "PT_ohmyverse_dcl_tools"
+    bl_idname = "OMV_PT_ohmyverse_dcl_tools"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'DCL'
@@ -14,26 +24,30 @@ class PT_OhmyverseDclTools(bpy.types.Panel):
     def draw(self, context):
         pass
 
-class PT_Armatures(bpy.types.Panel):
+class OMV_PT_Armatures(bpy.types.Panel):
     bl_label = "Armatures"
-    bl_idname = "PT_armatures_panel"
+    bl_idname = "OMV_PT_armatures_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'DCL'
-    bl_parent_id = "PT_ohmyverse_dcl_tools"
+    bl_parent_id = "OMV_PT_ohmyverse_dcl_tools"
  
     def draw(self,context):
         layout = self.layout
+        col = layout.column()
+        col.label(text="Select Armature:")
+        col.prop(context.scene, "Armature", text="")
+
         col = layout.column(align=True)
         col.operator_enum("dcl.armature_symmetrizer",  "action")
 
-class PT_VertexWeights(bpy.types.Panel):
+class OMV_PT_VertexWeights(bpy.types.Panel):
     bl_label = "Vertex Weights"
-    bl_idname = "PT_vertexweights_panel"
+    bl_idname = "OMV_PT_vertexweights_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'DCL'
-    bl_parent_id = "PT_ohmyverse_dcl_tools"
+    bl_parent_id = "OMV_PT_ohmyverse_dcl_tools"
  
     def draw(self,context):
         layout = self.layout
@@ -45,15 +59,26 @@ class PT_VertexWeights(bpy.types.Panel):
         else:   
             col.operator("dcl.weights_symmetrizer", text="Selected pose bones")
 
+def type_object_poll(self, object):
+    return object.type == 'ARMATURE'
+
+
 ####################################
 # REGISTER/UNREGISTER
 ####################################
 def register():
-    bpy.utils.register_class(PT_OhmyverseDclTools) 
-    bpy.utils.register_class(PT_Armatures) 
-    bpy.utils.register_class(PT_VertexWeights) 
+    bpy.utils.register_class(OMV_PT_OhmyverseDclTools) 
+    bpy.utils.register_class(OMV_PT_Armatures) 
+    bpy.utils.register_class(OMV_PT_VertexWeights) 
+
+    bpy.types.Scene.Armature = bpy.props.PointerProperty(
+        type=bpy.types.Object,
+        poll=type_object_poll
+    )
         
 def unregister():
-    bpy.utils.unregister_class(PT_OhmyverseDclTools)
-    bpy.utils.unregister_class(PT_Armatures)
-    bpy.utils.unregister_class(PT_VertexWeights)
+    bpy.utils.unregister_class(OMV_PT_OhmyverseDclTools)
+    bpy.utils.unregister_class(OMV_PT_Armatures)
+    bpy.utils.unregister_class(OMV_PT_VertexWeights)
+
+    del bpy.types.Scene.Armature
